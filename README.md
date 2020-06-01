1620082b5241b501805d9b3119dadc93f33ec13e
David <davidgalet@gmail.com>
2020-06-01 11:25:51 +0200

    upd core and add file ocmod

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# Opencart-Webp-Optimizer
- 
+# Opencart-Webp-Optimizer OC 3.X
+
+---
+
+This is Open-surce Version. This extension integrates WebP into OpenCart, and adds WebP images to the page if the browser supports it. Browser support for WebP is detected based on a user-agent check. If WebP support is detected, the extension will output webp format images, otherwise the original cached images(png, jpg, gif) will be displayed.
+
+---
+
+## Installation
+
+```text
+- This is a plug and play modification without any settings.
+- Install file .ocmod on Extensions/Installer; 
+- Reload on Extensions/Modifications;
+- PHP 7.x It's required
+```
+---
+
+## Edit compression image
+
+On /system/library/webpimage.php
+Edit the lines below to change the compression levels.
+```text
+private $options = [
+        'png' => [
+            'encoding' => 'auto',    /* Try both lossy and lossless and pick smallest */
+            'near-lossless' => 60,   /* The level of near-lossless image preprocessing (when trying lossless) */
+            'quality' => 85,         /* Quality when trying lossy. It is set high because pngs is often selected to ensure high quality */
+            'metadata' => 'none'     /* Meta data info img */
+        ],
+        'jpeg' => [
+            'encoding' => 'auto',     /* If you are worried about the longer conversion time, you could set it to "lossy" instead (lossy will often be smaller than lossless for jpegs) */
+            'quality' => 'auto',      /* Set to same as jpeg (requires imagick or gmagick extension, not necessarily compiled with webp) */
+            'max-quality' => 80,      /* Only relevant if quality is set to "auto" */
+            'default-quality' => 75,  /* Fallback quality if quality detection isnt working */
+            'metadata' => 'none'     /* Meta data info img */
+        ]
+    ];
+```
+
+---
+## How to contribute
+
+Fork the repository, edit and submit a pull request. Please be very clear on your commit messages and pull request, empty pull request messages may be rejected without reason. Your code standards should match the OpenCart coding standards. We use an automated code scanner to check for most basic mistakes - if the test fails your pull request will be rejected.
+
+---
+## Functionality
+
+- Compress image .jpg or .jpeg or .png
+- Transform to .webp and save cache copy
+- Library tool convert by https://github.com/rosell-dk/webp-convert
+
+---
+
+## Version
+
+1.0.0
+- First release
---
+++ image_dev.php
@@ -0,0 +1,80 @@
+<?php
+class ModelToolImage extends Model {
+
+	public function __construct($registry) {
+		parent::__construct($registry);
+		$registry->set('webp', new Webpimage($registry));
+	}
+
+	public function resize($filename, $width, $height) {
+		if (!is_file(DIR_IMAGE . $filename) || substr(str_replace('\\', '/', realpath(DIR_IMAGE . $filename)), 0, strlen(DIR_IMAGE)) != str_replace('\\', '/', DIR_IMAGE)) {
+			return;
+		}
+
+		$extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+		$image_old = $filename;
+		$image_new = 'cache/' . utf8_substr($filename, 0, utf8_strrpos($filename, '.')) . '-' . (int)$width . 'x' . (int)$height . '.' . $extension;
+		$image_webp = 'cachewebp/' . utf8_substr($filename, 0, utf8_strrpos($filename, '.')) . '-' . (int)$width . 'x' . (int)$height . '.webp';
+
+		if( $extension == 'png' || $extension == 'jpg' || $extension == 'jpeg'){
+			if (is_file(DIR_IMAGE . $image_new)) {
+				if (is_file(DIR_IMAGE . $image_webp) == false) {
+
+					$path = '';
+		
+					$directories = explode('/', dirname($image_webp));
+	
+					foreach ($directories as $directory) {
+						$path = $path . '/' . $directory;
+		
+						if (!is_dir(DIR_IMAGE . $path)) {
+							@mkdir(DIR_IMAGE . $path, 0777);
+						}
+					}
+					$this->webp->converter(DIR_IMAGE . $image_new,DIR_IMAGE . $image_webp);
+				}
+			}
+		}
+
+		if (is_file(DIR_IMAGE . $image_webp)) {
+			$image_new = $image_webp;
+		} else {
+			if (!is_file(DIR_IMAGE . $image_new) || (filemtime(DIR_IMAGE . $image_old) > filemtime(DIR_IMAGE . $image_new))) {
+				list($width_orig, $height_orig, $image_type) = getimagesize(DIR_IMAGE . $image_old);
+					 
+				if (!in_array($image_type, array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF))) { 
+					return DIR_IMAGE . $image_old;
+				}
+							
+				$path = '';
+	
+				$directories = explode('/', dirname($image_new));
+	
+				foreach ($directories as $directory) {
+					$path = $path . '/' . $directory;
+	
+					if (!is_dir(DIR_IMAGE . $path)) {
+						@mkdir(DIR_IMAGE . $path, 0777);
+					}
+				}
+	
+				if ($width_orig != $width || $height_orig != $height) {
+					$image = new Image(DIR_IMAGE . $image_old);
+					$image->resize($width, $height);
+					$image->save(DIR_IMAGE . $image_new);
+				} else {
+					copy(DIR_IMAGE . $image_old, DIR_IMAGE . $image_new);
+				}
+			}
+		}
+		
+		$image_new = str_replace(' ', '%20', $image_new);  // fix bug when attach image on email (gmail.com). it is automatic changing space " " to +
+		
+		if ($this->request->server['HTTPS']) {
+			return $this->config->get('config_ssl') . 'image/' . $image_new;
+		} else {
+			return $this->config->get('config_url') . 'image/' . $image_new;
+		}
+	}
+}
